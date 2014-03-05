d565d0cc3574ea3c59dd3c675089102deb4bc235
Elijah McNeish <elijahmcneish@gmail.com>
2014-03-05 16:03:15 -0600

    Get user input.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 #include "polynomial.h"
 
+Polynomial create_poly(int);
+
+Polynomial create_poly(int n_deg)
+{
+    Polynomial poly;
+    float coef;
+    while (n_deg >= 0)
+    {
+	std::cout << "x ^ " << n_deg << " coefficient: ";
+	std::cin >> coef;
+	poly.appendTerm(coef, n_deg);
+	--n_deg;
+    }
+    return poly;
+}
+
 int main()
 {
-    Polynomial p1;
-    p1.appendTerm(-2,6);
-    p1.appendTerm(3.6,4);
-    p1.appendTerm(5,3);
-    p1.appendTerm(2,1);
-    p1.appendTerm(1,0);
+    int left_deg, right_deg;
+    std::cout << "Enter highest degree of first polynomial: ";
+    std::cin >> left_deg;
+    Polynomial left_eq = create_poly(left_deg);
+    std::cout << "Enter highest degree of second polynomial: ";
+    std::cin >> right_deg;
+    Polynomial right_eq = create_poly(right_deg);
 
-    Polynomial p2;
-    p2.appendTerm(2,6);
-    p2.appendTerm(5,5);
-    p2.appendTerm(-1.2,1);
-    p2.appendTerm(1,0);
-
-    Polynomial result = p1.sum(p2);
+    Polynomial result = left_eq.sum(right_eq);
     result.printTerms();
-    
+
     return 0;
 }
